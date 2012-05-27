0f16ee048f31dff2ea7905c643c4c36f794ec68b
koduki <pascalm3@gmail.com>
2012-05-27 21:26:30 +0900

    add find recent articles impl.

--- src/main/java/cn/orz/pascal/blog/dao/ArticleFacade.java
+++ src/main/java/cn/orz/pascal/blog/dao/ArticleFacade.java
@@ -5,15 +5,11 @@
 package cn.orz.pascal.blog.dao;
 
 import cn.orz.pascal.blog.entity.Article;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -35,19 +31,10 @@ public class ArticleFacade extends AbstractFacade<Article> {
     }
 
     public List<Article> findRecently(int size) {
-        List<Article> result = new ArrayList<Article>(size);
-        for (int i = 90; i < 100; i++) {
-            try {
-                SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+        String jpql = "SELECT a FROM Article a ORDER BY a.updatedAt DESC";
+        TypedQuery<Article> query = em.createQuery(jpql, Article.class).
+                setMaxResults(size);
 
-                Article article = new Article(null, "title" + i, "contents" + i);
-                article.setCreatedAt(df.parse("2012/04/1"));
-                article.setUpdatedAt(df.parse((2012 + i) + "/04/1"));
-                result.add(article);
-            } catch (ParseException ex) {
-                Logger.getLogger(ArticleFacade.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        return result;
+        return query.getResultList();
     }
 }
